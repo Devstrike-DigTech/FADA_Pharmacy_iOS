8c8e734adce3d6e88c40efb7efb44c68c2a49891
Richard Uzor <43550320+EQua-Dev@users.noreply.github.com>
2024-01-22 21:47:24 +0100

    Initial Commit

---
+++ FADA Pharmacy/FADA_PharmacyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FADA_PharmacyApp.swift
+//  FADA Pharmacy
+//
+//  Created by Richard Uzor on 22/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct FADA_PharmacyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
